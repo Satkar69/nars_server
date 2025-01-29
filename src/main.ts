debb7ca195b931620e122e900e10dd7c98b42ee4
SatkarNiraula <satkarniraula69@gmail.com>
2025-01-29 18:38:27 +0545

    update :: add new cors origin

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
 
   // -- Cors setup
   app.enableCors({
-    origin: ['*', 'http://localhost:3001'],
+    origin: ['*', 'http://localhost:3001', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
