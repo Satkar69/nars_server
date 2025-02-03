6878004c1d270aac8d55dcf18d0170aeb846ad31
SatkarNiraula <satkarniraula69@gmail.com>
2025-02-03 19:46:35 +0545

    update :: get all hospitals route from the static json file

---
+++ src/application/controllers/hospital/hospital.contoller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { CoreApiResponse } from 'src/application/api/core-api-response';
+import { Public } from 'src/application/decorators/public.decorator';
+const hospitalData = require('../../../data/hospital-data.json');
+
+@Public()
+@Controller()
+export class HospitalController {
+  constructor() {}
+
+  @Get('/get-all')
+  async getAllHospitals() {
+    return CoreApiResponse.success(hospitalData);
+  }
+}
