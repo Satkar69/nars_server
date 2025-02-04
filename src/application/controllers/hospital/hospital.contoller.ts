d114ad1fc781bef914807571a864bb91b9b79340
SatkarNiraula <satkarniraula69@gmail.com>
2025-02-04 13:40:40 +0545

    update ;

--- src/application/controllers/hospital/hospital.contoller.ts
+++ src/application/controllers/hospital/hospital.contoller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { CoreApiResponse } from 'src/application/api/core-api-response';
 import { Public } from 'src/application/decorators/public.decorator';
-const hospitalData = require('../../../data/hospital-data.json');
+import hospitalData from '../../../data/hospital-data.json';
 
 @Public()
 @Controller()
